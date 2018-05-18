b9a3e1db09599d72736dd4d78fbf8ee142f2b8dd
zhihua cai <zhihua cai>
2018-05-18 19:39:07 +0800

    ch5:structure

--- manage.py
+++ manage.py
@@ -1,12 +1,21 @@
+import os
+
 from flask_script import Manager, Server
+from flask_script.commands import ShowUrls
 from flask_migrate import Migrate, MigrateCommand
 
-from main import app, db, User, Post, Tag, Comment
+from webapp import create_app
+from webapp.models import db, User, Post, Tag, Comment
+
+# default to dev config
+env = os.environ.get('WEBAPP_ENV', 'dev')
+app = create_app('webapp.config.%sConfig' % env.capitalize())
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command("server", Server())
+manager.add_command("show-urls", ShowUrls())
 manager.add_command('db', MigrateCommand)
 
 
